52db6b028ea464827efeb1a24281227e9ac1c269
Julio Ortiz <kid.robot.ard@gmail.com>
2019-08-02 15:43:23 -0500

    Subi el proyecto inicial de unity

---
+++ Proyecto Mi juego 2d/Assets/scripts/EstructurasDeControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstructurasDeControl : MonoBehaviour
+{
+    public int vida = 100 ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (vida > 50)
+        {
+
+            //PONER ANIMACION DE JUGADOR SANO
+            Debug.Log("El jugador tiene vida mayor a 50");
+        }
+        else if(vida < 30)
+        {
+            // PONER ANIMACION DE JUGADOR HERIDO
+
+            Debug.Log("El jugador esta jodido");
+        }
+        else
+        {
+            // PONER ANIMACION DE JUGADOR MUERTO
+
+            Debug.Log("El jugador esta muerto");
+        }
+
+
+     }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("PLAY GAME");
+    }
+
+    public void ShowCredits()
+    {
+        Debug.Log("CREDITS");
+    }
+
+    public void Exitgames()
+    {
+        Debug.Log("EXIT");
+    }
+
+}
---
+++ Proyecto Mi juego 2d/Assets/scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    float jumpSpeed = 5F;
+    float runSpeed = 5F;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeetCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent <Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        flipSprite();
+        Jump();
+    }
+
+    void Run()
+    {
+
+        //OBTENER EL FLOAT(DECIMAL) DEL CONTROL QUE VA DE -1 A 1
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        //INICIALIZAR UN VECTOR DE DOS DIMENSIONES QUE SOLO MODIFICA EL COMPONENTE X
+        Vector2 playerVelocity = new Vector2(controlThrow*runSpeed,myRigidbody.velocity.y);
+
+        //ASIGNAR LA NUEVA VELOCIDAD A MI RIGID BODY
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > 0;
+
+        //APLICAR ANIMACION DE CORRER SETEANDO LA CONDICION DE "RUNNING" DEL ANIMATOR
+
+        if (playerHasHorizontalSpeed)
+        {
+            myAnimator.SetBool("Running", true);
+        }
+
+       else
+        {
+            myAnimator.SetBool("Running", false);
+        }
+    }
+
+    void flipSprite()
+
+        //VERIFICAR SI EXITE VELOCIDAD EN X INDEPENDIENTEMENTE DEL SIGNO (POR ESO USAMOS VALOR ABSOLUTO)
+        // GUARDAMOS ESTA VERIFICACION 
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs (myRigidbody.velocity.x) > 0;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+
+
+    }
+
+    void Jump()
+    {
+
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+
+        {
+            return;
+        }
+
+        //OBTENER EL BOLEANO (TRUE/FALSE) DEL BOTON REPRESENTADO POR EL TAG "JUMP"
+        bool isJumpButtonPressed = CrossPlatformInputManager.GetButtonDown("Jump");
+        if (isJumpButtonPressed)
+
+        {
+
+            Vector2 junpVelocity = new Vector2(0, jumpSpeed);
+            //SUMARLE A LA VELOCIDAD QUE YA TIENE MI NUEVO VECTOR DE VELOCIDAD
+            myRigidbody.velocity += junpVelocity;
+        }
+
+    }
+}
